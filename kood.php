26118667ba565fef6434ae5c52df37f3f7521c83
krisveen <krisveen@greeny.cs.tlu.ee>
2016-09-14 14:48:50 +0300

    added if and for loop

---
+++ kood.php
@@ -0,0 +1,22 @@
+<?php
+	$age=5;
+	
+	if ($age < 18) {
+		echo "alaealine";
+	} else {
+		echo "t2isealine";
+	}
+
+?>
+
+<br>
+
+<?php
+	for($i = 1; $i <= $age; $i=$i+1 ){
+		echo $i."palju";
+		
+	}
+
+	echo "onne";
+
+?>
